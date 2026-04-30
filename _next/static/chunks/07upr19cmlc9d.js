3b5379929b68fcdbf8b99c158eaf3a5fa14e7c4f
Hazem <hazem@mersal.com>
2026-04-30 23:23:20 +0300

    Add compiled static CSS for Hostinger LiteSpeed compatibility

---
+++ _next/static/chunks/07upr19cmlc9d.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,45951,t=>{t.v(e=>Promise.all(["static/chunks/0s0ve61feturi.js"].map(e=>t.l(e))).then(()=>e(38147)))}]);
